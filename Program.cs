a3865e1cc17c0c306d00aff18b679b66057b05e1
muratpala007 <muratpala007@gmail.com>
2022-04-19 16:32:20 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _57DersDizilerinBaziOzelikleriVeMetotlari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayilar = { 2, 3, 10};
+            int[,,,,,] sayilar2 = new int[2, 2, 2, 3, 4, 5];
+            Console.WriteLine(sayilar.Length); //Dizinin eleman sayisi
+            Console.WriteLine(sayilar2.Rank); //Dizinin kac boyutlu oldugunu soyler
+            Console.WriteLine(sayilar.Max()); //Dizideki en buyuk elemani verir
+            Console.WriteLine(sayilar.Min()); //Dizideki en kucuk elemani verir
+            Console.WriteLine(sayilar.Sum()); //Dizideki elemanlarin toplamini verir
+        }
+    }
+}
